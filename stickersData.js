9e18adedef39156d0dbabdf6ae1830b42d4a840e
nauxscript <nauxscript@gmail.com>
2022-12-01 21:40:30 +0800

    feat: updated basic stickers data

---
+++ stickersData.js
@@ -0,0 +1,289 @@
+export default {
+  "data": [
+    {
+      "code": "[旺柴]",
+      "image": "",
+      "keyword": "doge 狗"
+    },
+    {
+      "keyword": "微笑",
+      "code": "/::)"
+    },
+    {
+      "keyword": "伤心",
+      "code": "/::~"
+    },
+    {
+      "keyword": "美女",
+      "code": "/::B"
+    },
+    {
+      "keyword": "发呆",
+      "code": "/::|"
+    },
+    {
+      "keyword": "墨镜",
+      "code": "/:8-)"
+    },
+    {
+      "keyword": "哭",
+      "code": "/::<"
+    },
+    {
+      "keyword": "羞",
+      "code": "/::$"
+    },
+    {
+      "keyword": "哑",
+      "code": "/::X"
+    },
+    {
+      "keyword": "睡",
+      "code": "/::Z"
+    },
+    {
+      "keyword": "哭",
+      "code": "/::’("
+    },
+    {
+      "keyword": "囧",
+      "code": "/::-|"
+    },
+    {
+      "keyword": "怒",
+      "code": "/::@"
+    },
+    {
+      "keyword": "调皮",
+      "code": "/::P"
+    },
+    {
+      "keyword": "笑",
+      "code": "/::D"
+    },
+    {
+      "keyword": "惊讶",
+      "code": "/::O"
+    },
+    {
+      "keyword": "难过",
+      "code": "/::("
+    },
+    {
+      "keyword": "酷",
+      "code": "/::+"
+    },
+    {
+      "keyword": "汗",
+      "code": "/:–b"
+    },
+    {
+      "keyword": "抓狂",
+      "code": "/::Q"
+    },
+    {
+      "keyword": "吐",
+      "code": "/::T"
+    },
+    {
+      "keyword": "笑",
+      "code": "/:,@P"
+    },
+    {
+      "keyword": "快乐",
+      "code": "/:,@-D"
+    },
+    {
+      "keyword": "奇",
+      "code": "/::d"
+    },
+    {
+      "keyword": "傲",
+      "code": "/:,@o"
+    },
+    {
+      "keyword": "饿",
+      "code": "/::g"
+    },
+    {
+      "keyword": "累",
+      "code": "/:|-)"
+    },
+    {
+      "keyword": "吓",
+      "code": "/::!"
+    },
+    {
+      "keyword": "汗",
+      "code": "/::L"
+    },
+    {
+      "keyword": "高兴",
+      "code": "/::>"
+    },
+    {
+      "keyword": "闲",
+      "code": "/::,@"
+    },
+    {
+      "keyword": "努力",
+      "code": "/:,@f"
+    },
+    {
+      "keyword": "骂",
+      "code": "/::-S"
+    },
+    {
+      "keyword": "疑问",
+      "code": "/:?"
+    },
+    {
+      "keyword": "秘密",
+      "code": "/:,@x"
+    },
+    {
+      "keyword": "乱",
+      "code": "/:,@@"
+    },
+    {
+      "keyword": "疯",
+      "code": "/::8"
+    },
+    {
+      "keyword": "哀",
+      "code": "/:,@!"
+    },
+    {
+      "keyword": "鬼",
+      "code": "/:!!!"
+    },
+    {
+      "keyword": "打击",
+      "code": "/:xx"
+    },
+    {
+      "keyword": "bye",
+      "code": "/:bye"
+    },
+    {
+      "keyword": "汗",
+      "code": "/:wipe"
+    },
+    {
+      "keyword": "抠",
+      "code": "/:dig"
+    },
+    {
+      "keyword": "鼓掌",
+      "code": "/:handclap"
+    },
+    {
+      "keyword": "糟糕",
+      "code": "/:&-("
+    },
+    {
+      "keyword": "恶搞",
+      "code": "/:B-)"
+    },
+    {
+      "keyword": "什么",
+      "code": "/:<@"
+    },
+    {
+      "keyword": "什么",
+      "code": "/:@>"
+    },
+    {
+      "keyword": "累",
+      "code": "/::-O"
+    },
+    {
+      "keyword": "看",
+      "code": "/:>-|"
+    },
+    {
+      "keyword": "难过",
+      "code": "/:P-("
+    },
+    {
+      "keyword": "难过",
+      "code": "/::’|"
+    },
+    {
+      "keyword": "坏",
+      "code": "/:X-)"
+    },
+    {
+      "keyword": "亲",
+      "code": "/::*"
+    },
+    {
+      "keyword": "吓",
+      "code": "/:@x"
+    },
+    {
+      "keyword": "可怜",
+      "code": "/:8*"
+    },
+    {
+      "keyword": "刀",
+      "code": "/:pd"
+    },
+    {
+      "keyword": "水果",
+      "code": "/:<W>"
+    },
+    {
+      "keyword": "酒",
+      "code": "/:beer"
+    },
+    {
+      "keyword": "篮球",
+      "code": "/:basketb"
+    },
+    {
+      "keyword": "乒乓",
+      "code": "/:oo"
+    },
+    {
+      "keyword": "咖啡",
+      "code": "/:coffee"
+    },
+    {
+      "keyword": "美食",
+      "code": "/:eat"
+    },
+    {
+      "keyword": "动物",
+      "code": "/:pig"
+    },
+    {
+      "keyword": "鲜花",
+      "code": "/:rose"
+    },
+    {
+      "keyword": "枯",
+      "code": "/:fade"
+    },
+    {
+      "keyword": "唇",
+      "code": "/:showlove"
+    },
+    {
+      "keyword": "爱",
+      "code": "/:heart"
+    },
+    {
+      "keyword": "分手",
+      "code": "/:break"
+    },
+    {
+      "keyword": "生日",
+      "code": "/:cake"
+    },
+    {
+      "keyword": "电",
+      "code": "/:li"
+    },
+  ]
+}
