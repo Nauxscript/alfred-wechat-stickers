bf007c0efa5c39cf3143163e448f26430276cca7
nauxscript <nauxscript@gmail.com>
2022-12-01 21:40:51 +0800

    feat: basic search logic

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import alfy from 'alfy';
+import stickersData from './stickersData.js'
 
-const data = await alfy.fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await alfy.fetch('https://jsonplaceholder.typicode.com/posts');
+const data = JSON.parse(JSON.stringify(stickersData)).data
 
 const items = alfy
-	.inputMatches(data, 'title')
+	.inputMatches(data, 'keyword')
 	.map(element => ({
-		title: element.title,
-		subtitle: element.body,
-		arg: element.id
+		title: element.code,
+		subtitle: element.keyword,
+		arg: element.code,
+		mods: {
+			ctrl: {
+				arg: element.code,
+				subtitle: 'Copy and paste',
+			},
+		},
 	}));
 
 alfy.output(items);
