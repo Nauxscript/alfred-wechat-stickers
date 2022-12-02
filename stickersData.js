3b99f2d1dfe76078501f7092d578403f2f0859f4
nauxscript <nauxscript@gmail.com>
2022-12-02 10:24:13 +0800

    chore: updating keywords and names

--- stickersData.js
+++ stickersData.js
@@ -5,427 +5,427 @@ export default {
     {
       "code": "[旺柴]",
       "name": "旺柴",
-      "key": "doge",
+      "key": "旺柴",
       "keyword": "doge 狗"
     },
     {
-      "keyword": "微笑",
+      "keyword": "微笑 smile wx",
       "name": "微笑",
-      "key": "smile",
+      "key": "微笑",
       "code": "/::)"
     },
     {
-      "keyword": "伤心",
+      "keyword": "伤心 sad sx",
       "name": "伤心",
-      "key": "sad",
+      "key": "伤心",
       "code": "/::~"
     },
     {
-      "keyword": "美女",
+      "keyword": "美女 beauty ml",
       "name": "美女",
-      "key": "beauty",
+      "key": "美女",
       "code": "/::B"
     },
     {
-      "keyword": "发呆",
+      "keyword": "发呆 daze 楞 fd",
       "name": "发呆",
-      "key": "daze",
+      "key": "发呆",
       "code": "/::|"
     },
     {
-      "keyword": "墨镜",
+      "keyword": "墨镜 眼镜 yj glass",
       "name": "墨镜",
       "key": "sunglasses",
       "code": "/:8-)"
     },
     {
-      "keyword": "哭",
-      "name": "哭",
-      "key": "cry",
+      "keyword": "大哭 cry dk",
+      "name": "大哭",
+      "key": "大哭",
       "code": "/::<"
     },
     {
-      "keyword": "羞",
-      "name": "羞",
-      "key": "",
+      "keyword": "害羞 shy hx",
+      "name": "害羞",
+      "key": "害羞",
       "code": "/::$"
     },
     {
-      "keyword": "哑",
-      "name": "哑",
-      "key": "",
+      "keyword": "闭嘴 哑 bz ya",
+      "name": "闭嘴",
+      "key": "闭嘴",
       "code": "/::X"
     },
     {
-      "keyword": "睡",
-      "name": "睡",
-      "key": "",
+      "keyword": "睡觉 sleep 晚安 night wa",
+      "name": "睡觉",
+      "key": "睡觉",
       "code": "/::Z"
     },
     {
-      "keyword": "哭",
-      "name": "哭",
-      "key": "",
+      "keyword": "哭泣 呜呜呜 流泪",
+      "name": "哭泣",
+      "key": "哭泣",
       "code": "/::’("
     },
     {
-      "keyword": "囧",
+      "keyword": "囧 无语 尴尬 gg wy jiong",
       "name": "囧",
-      "key": "",
+      "key": "囧",
       "code": "/::-|"
     },
     {
       "keyword": "怒",
       "name": "怒",
-      "key": "",
+      "key": "怒",
       "code": "/::@"
     },
     {
       "keyword": "调皮",
       "name": "调皮",
-      "key": "",
+      "key": "调皮",
       "code": "/::P"
     },
     {
-      "keyword": "笑",
-      "name": "笑",
-      "key": "",
+      "keyword": "呲牙笑 cyx 笑",
+      "name": "呲牙笑",
+      "key": "呲牙笑",
       "code": "/::D"
     },
     {
-      "keyword": "惊讶",
+      "keyword": "惊讶 jy surprised",
       "name": "惊讶",
-      "key": "",
+      "key": "惊讶",
       "code": "/::O"
     },
     {
-      "keyword": "难过",
+      "keyword": "难过 ng sad",
       "name": "难过",
-      "key": "",
+      "key": "难过",
       "code": "/::("
     },
     {
-      "keyword": "酷",
+      "keyword": "酷 cool ku",
       "name": "酷",
-      "key": "",
+      "key": "酷",
       "code": "/::+"
     },
     {
       "keyword": "汗",
       "name": "汗",
-      "key": "",
+      "key": "汗",
       "code": "/:–b"
     },
     {
       "keyword": "抓狂",
       "name": "抓狂",
-      "key": "",
+      "key": "抓狂",
       "code": "/::Q"
     },
     {
       "keyword": "吐",
       "name": "吐",
-      "key": "",
+      "key": "吐",
       "code": "/::T"
     },
     {
-      "keyword": "笑",
-      "name": "笑",
-      "key": "",
+      "keyword": "捂嘴笑 wzx",
+      "name": "捂嘴笑",
+      "key": "捂嘴笑",
       "code": "/:,@P"
     },
     {
       "keyword": "快乐",
       "name": "快乐",
-      "key": "",
+      "key": "快乐",
       "code": "/:,@-D"
     },
     {
       "keyword": "奇",
       "name": "奇",
-      "key": "",
+      "key": "奇",
       "code": "/::d"
     },
     {
       "keyword": "傲",
       "name": "傲",
-      "key": "",
+      "key": "傲",
       "code": "/:,@o"
     },
     {
       "keyword": "饿",
       "name": "饿",
-      "key": "",
+      "key": "饿",
       "code": "/::g"
     },
     {
       "keyword": "累",
       "name": "累",
-      "key": "",
+      "key": "累",
       "code": "/:|-)"
     },
     {
       "keyword": "吓",
       "name": "吓",
-      "key": "",
+      "key": "吓",
       "code": "/::!"
     },
     {
       "keyword": "汗",
       "name": "汗",
-      "key": "",
+      "key": "汗",
       "code": "/::L"
     },
     {
       "keyword": "高兴",
       "name": "高兴",
-      "key": "",
+      "key": "高兴",
       "code": "/::>"
     },
     {
       "keyword": "闲",
       "name": "闲",
-      "key": "",
+      "key": "闲",
       "code": "/::,@"
     },
     {
       "keyword": "努力",
       "name": "努力",
-      "key": "",
+      "key": "努力",
       "code": "/:,@f"
     },
     {
       "keyword": "骂",
       "name": "骂",
-      "key": "",
+      "key": "骂",
       "code": "/::-S"
     },
     {
       "keyword": "疑问",
       "name": "疑问",
-      "key": "",
+      "key": "疑问",
       "code": "/:?"
     },
     {
       "keyword": "秘密",
       "name": "秘密",
-      "key": "",
+      "key": "秘密",
       "code": "/:,@x"
     },
     {
       "keyword": "乱",
       "name": "乱",
-      "key": "",
+      "key": "乱",
       "code": "/:,@@"
     },
     {
       "keyword": "疯",
       "name": "疯",
-      "key": "",
+      "key": "疯",
       "code": "/::8"
     },
     {
       "keyword": "哀",
       "name": "哀",
-      "key": "",
+      "key": "哀",
       "code": "/:,@!"
     },
     {
       "keyword": "鬼",
       "name": "鬼",
-      "key": "",
+      "key": "鬼",
       "code": "/:!!!"
     },
     {
       "keyword": "打击",
       "name": "打击",
-      "key": "",
+      "key": "打击",
       "code": "/:xx"
     },
     {
       "keyword": "bye",
       "name": "bye",
-      "key": "",
+      "key": "bye",
       "code": "/:bye"
     },
     {
       "keyword": "汗",
       "name": "汗",
-      "key": "",
+      "key": "汗",
       "code": "/:wipe"
     },
     {
       "keyword": "抠",
       "name": "抠",
-      "key": "",
+      "key": "抠",
       "code": "/:dig"
     },
     {
       "keyword": "鼓掌",
       "name": "鼓掌",
-      "key": "",
+      "key": "鼓掌",
       "code": "/:handclap"
     },
     {
       "keyword": "糟糕",
       "name": "糟糕",
-      "key": "",
+      "key": "糟糕",
       "code": "/:&-("
     },
     {
       "keyword": "恶搞",
       "name": "恶搞",
-      "key": "",
+      "key": "恶搞",
       "code": "/:B-)"
     },
     {
       "keyword": "什么",
       "name": "什么",
-      "key": "",
+      "key": "什么",
       "code": "/:<@"
     },
     {
       "keyword": "什么",
       "name": "什么",
-      "key": "",
+      "key": "什么",
       "code": "/:@>"
     },
     {
       "keyword": "累",
       "name": "累",
-      "key": "",
+      "key": "累",
       "code": "/::-O"
     },
     {
       "keyword": "看",
       "name": "看",
-      "key": "",
+      "key": "看",
       "code": "/:>-|"
     },
     {
       "keyword": "难过",
       "name": "难过",
-      "key": "",
+      "key": "难过",
       "code": "/:P-("
     },
     {
       "keyword": "难过",
       "name": "难过",
-      "key": "",
+      "key": "难过",
       "code": "/::’|"
     },
     {
       "keyword": "坏",
       "name": "坏",
-      "key": "",
+      "key": "坏",
       "code": "/:X-)"
     },
     {
       "keyword": "亲",
       "name": "亲",
-      "key": "",
+      "key": "亲",
       "code": "/::*"
     },
     {
       "keyword": "吓",
       "name": "吓",
-      "key": "",
+      "key": "吓",
       "code": "/:@x"
     },
     {
       "keyword": "可怜",
       "name": "可怜",
-      "key": "",
+      "key": "可怜",
       "code": "/:8*"
     },
     {
       "keyword": "刀",
       "name": "刀",
-      "key": "",
+      "key": "刀",
       "code": "/:pd"
     },
     {
       "keyword": "水果",
       "name": "水果",
-      "key": "",
+      "key": "水果",
       "code": "/:<W>"
     },
     {
       "keyword": "酒",
       "name": "酒",
-      "key": "",
+      "key": "酒",
       "code": "/:beer"
     },
     {
       "keyword": "篮球",
       "name": "篮球",
-      "key": "",
+      "key": "篮球",
       "code": "/:basketb"
     },
     {
       "keyword": "乒乓",
       "name": "乒乓",
-      "key": "",
+      "key": "乒乓",
       "code": "/:oo"
     },
     {
       "keyword": "咖啡",
       "name": "咖啡",
-      "key": "",
+      "key": "咖啡",
       "code": "/:coffee"
     },
     {
       "keyword": "美食",
       "name": "美食",
-      "key": "",
+      "key": "美食",
       "code": "/:eat"
     },
     {
       "keyword": "动物",
       "name": "动物",
-      "key": "",
+      "key": "动物",
       "code": "/:pig"
     },
     {
       "keyword": "鲜花",
       "name": "鲜花",
-      "key": "",
+      "key": "鲜花",
       "code": "/:rose"
     },
     {
       "keyword": "枯",
       "name": "枯",
-      "key": "",
+      "key": "枯",
       "code": "/:fade"
     },
     {
       "keyword": "唇",
       "name": "唇",
-      "key": "",
+      "key": "唇",
       "code": "/:showlove"
     },
     {
       "keyword": "爱",
       "name": "爱",
-      "key": "",
+      "key": "爱",
       "code": "/:heart"
     },
     {
       "keyword": "分手",
       "name": "分手",
-      "key": "",
+      "key": "分手",
       "code": "/:break"
     },
     {
       "keyword": "生日",
       "name": "生日",
-      "key": "",
+      "key": "生日",
       "code": "/:cake"
     },
     {
       "keyword": "电",
       "name": "电",
-      "key": "",
+      "key": "电",
       "code": "/:li"
     },
   ]
