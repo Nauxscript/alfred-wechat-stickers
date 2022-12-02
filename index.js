e1d793ac50955d75222b50fd4da57c39ae474a45
nauxscript <nauxscript@gmail.com>
2022-12-02 14:38:55 +0800

    feat: removed dependency alfy

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ const iconPath = './icons'
 // const data = await alfy.fetch('https://jsonplaceholder.typicode.com/posts');
 const data = JSON.parse(JSON.stringify(stickersData)).data
 
-const items = alfy
-	.inputMatches(data, 'keyword')
+let keyword = process.argv[2]
+
+// console.log(process.argv[2]) 
+
+const items = data
+	.filter(element => element.keyword.includes(keyword))
 	.map(element => ({
 		title: element.name,
 		subtitle: element.keyword,
 		arg: element.code,
 		icon: {
-			path: element.key ? `${iconPath}/${element.key}.png` : `${iconPath}/default.png` 
+			path: element.key ? `${iconPath}/${element.key}.png` : `${iconPath}/default.png`
 		},
 		mods: {
 			ctrl: {
 				arg: element.code,
 				subtitle: 'Copy and paste',
 			},
-		},
-	}));
+		}
+	}))
 
-alfy.output(items);
+console.log(JSON.stringify({ items }, null, '\t'));
