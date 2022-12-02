a77f622f5e00c53663fcfe6595b2f9e0b712044e
nauxscript <nauxscript@gmail.com>
2022-12-02 10:24:37 +0800

    feat: show sticker preview in selection

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import alfy from 'alfy';
 import stickersData from './stickersData.js'
 
-const iconPath = process.cwd() + '/icons/'
+const iconPath = './icons'
 // const data = await alfy.fetch('https://jsonplaceholder.typicode.com/posts');
 const data = JSON.parse(JSON.stringify(stickersData)).data
 
 const items = alfy
 	.inputMatches(data, 'keyword')
 	.map(element => ({
-		title: element.code,
+		title: element.name,
 		subtitle: element.keyword,
 		arg: element.code,
 		icon: {
-			type: 'fileicon',
-			path: element.key ? `${iconPath}${element.key}.png` : `${iconPath}default.png` 
+			path: element.key ? `${iconPath}/${element.key}.png` : `${iconPath}/default.png` 
 		},
 		mods: {
 			ctrl: {
