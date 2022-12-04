bb640bbd32193e92cdc8e32b64fccf346006390e
nauxscript <nauxscript@gmail.com>
2022-12-04 11:26:35 +0800

    fix: fixed test mode interferes workflow running

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@
 
 import { searchFn } from './core'
 
-export default function run(argv: string[], forTest = false) {
+export default function run(argv: Array<string | boolean >) {
   const query = argv[0]
-  const items = searchFn(query)
+  const items = searchFn(query as string)
   const result = { items }
-  if (forTest)
+  if (argv[1]) {
+    // eslint-disable-next-line no-console
+    console.log(argv[1])
     return result
+  }
   // eslint-disable-next-line no-console
   console.log(JSON.stringify(result, null, ''))
 }
