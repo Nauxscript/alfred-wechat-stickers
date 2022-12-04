c558278b27ff95c55a5bf9344b5bc97257338617
nauxscript <nauxscript@gmail.com>
2022-12-04 15:43:07 +0800

    feat: output type & raw data

--- scripts/image2data.ts
+++ scripts/image2data.ts
@@ -10,11 +10,31 @@ const iconFiles = files.filter((icon) => {
   return path.extname(icon).toLowerCase() === '.png'
 })
 
-const fileString2jsonString = (fileObj: any) => {
-  const obj = { data: fileObj }
-  return JSON.stringify(obj).replace(/.png/g, '')
+const string2Export = (head: string, tail: string) => {
+  return `${head} = ${tail}`
 }
 
-fs.writeFile('./src/stickersData.json', fileString2jsonString(iconFiles), (err) => {
+// const fileString2jsonString = (fileObj: any) => {
+//   const obj = { data: fileObj }
+//   return JSON.stringify(obj).replace(/.png/g, '')
+// }
+
+const string2typeExport = (name: string, str: string) => {
+  return string2Export(`export type ${name}`, `${str}`)
+}
+
+const string2defaultExport = (valueStr: string, typeStr: string) => {
+  return `export default ${valueStr} as ${typeStr}`
+}
+
+const assembler = (keys: string[]) => {
+  const typeValueStr = keys.map(item => `'${item}'`).join(' | ')
+  const keyValueStr = JSON.stringify(keys)
+  const typeStr = string2typeExport('Keys', typeValueStr)
+  const valueStr = string2defaultExport(keyValueStr, 'Keys[]')
+  return `${typeStr}\r${valueStr}\r`.replace(/.png/g, '').replace(/\"/g, '\'').replace(/\,/g, ', ')
+}
+
+fs.writeFile('./src/stickersData.ts', assembler(iconFiles), (err) => {
   console.log(err)
 })
