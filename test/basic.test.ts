d455cdcd0b67994611e28d5b9014a7c2d526bb3c
nauxscript <nauxscript@gmail.com>
2022-12-04 10:05:23 +0800

    feat: add first test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,7 +1,13 @@
 import { describe, expect, it } from 'vitest'
+// import run from '../src/index'
 
-describe('start up', () => {
+describe('index entry', () => {
   it('happy path', () => {
     expect(1 + 1).toEqual(2)
   })
+
+  // it('query string is empty', () => {
+  //   const result = run([''], true)
+  //   expect(result).toMatchSnapshot()
+  // })
 })
