5cb93d2529aaf54683f9085cc3787cced527da81
nauxscript <nauxscript@gmail.com>
2022-12-04 10:57:19 +0800

    fix: field limit in keywordmap

--- src/keywordMap.ts
+++ src/keywordMap.ts
@@ -2,6 +2,9 @@ import type { Keys } from './stickersData'
 
 type KeywordMap = Record<string, Keys[]>
 
-export default {
-  '笑 smile': ['旺柴', '奸笑', '社会', '呲牙', '再见', '皱眉', '偷笑', '坏笑', '色', '憨笑', '微笑', '阴险', '愉快', '太阳'],
-} as KeywordMap
+const keywordMap: KeywordMap = {
+  '笑 smile': ['旺柴', '奸笑', '社会社会', '呲牙', '再见', '皱眉', '偷笑', '坏笑', '色', '憨笑', '微笑', '阴险', '愉快', '太阳'],
+}
+
+export default keywordMap
+
