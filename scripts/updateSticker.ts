f9c6d0c0e5c1749d774f6e83d5f77e0c0ca24cd4
nauxscript <nauxscript@gmail.com>
2022-12-30 15:32:54 +0800

    feat: Migrate icon image resource to a separate repo
    closed #14

---
+++ scripts/updateSticker.ts
@@ -0,0 +1,36 @@
+import https from 'node:https'
+import fs from 'fs'
+import download from 'download'
+
+const req = https.request({
+  host: 'api.github.com',
+  path: '/repos/Nauxscript/wechat-stickers-data/releases/latest',
+  method: 'GET',
+  headers: {
+    'User-Agent': 'Nauxscript',
+    'Content-Type': 'application/json; charset=utf-8',
+  },
+  rejectUnauthorized: false,
+}, (response) => {
+  let datas = ''
+  // called when a data chunk is received.
+  response.on('data', (chunk) => {
+    datas += chunk
+  })
+  // called when the complete response is received.
+  response.on('end', async () => {
+    console.log('done')
+    const repoInfo = JSON.parse(datas) as { tag_name: string }
+    const assetUrl = `https://github.com/Nauxscript/wechat-stickers-data/releases/download/${repoInfo.tag_name}/icons.zip`
+    console.log(assetUrl)
+    const data = await download(assetUrl)
+    await fs.promises.writeFile('./scripts/icons.zip', data)
+  })
+  console.log('Response statusCode:', response.statusCode)
+})
+
+req.on('error', (error) => {
+  console.log(`Error: ${error.message}`)
+})
+
+req.end()
